accca4a7d78a899054bd7c73d57e46edc5237e75
anfelder <andrewfelder95@gmail.com>
2015-10-10 18:36:28 -0500

    Implemented Login and Sign Up functionality

--- Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/LandingActivity.java
+++ Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/LandingActivity.java
@@ -1,5 +1,7 @@
 package com.dine_n_wineinc.dine_n_wine;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
@@ -34,12 +36,14 @@ public class LandingActivity extends AppCompatActivity implements Animation.Anim
     TextView textView;
     LinearLayout linearLayout; //Buttons on the start screen (Login, signup)
     RelativeLayout loginLayout, signupLayout;
-    //--------------------------------
     Animation animationSlideLeft; //Welcome offscreen for login
     Animation animationSlideRight; //Login
     Animation animationSlideLeftS; //Signup
     Animation animationSlideExit; //Welcome offscreen for signup
-    //---------------------------------
+    String loginPassword, loginEmail; //login EditText values
+    String SUPassword, SUEmail, SUFN, SULN;//sign up strings
+    EditText loginPasswordEditText, loginEmailEditText;
+    EditText SUPWEditText, SUEmailEditText, SUFNEditText,SULNEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,14 +56,12 @@ public class LandingActivity extends AppCompatActivity implements Animation.Anim
         loginLayout.setVisibility(View.INVISIBLE);
         signupLayout = (RelativeLayout)findViewById(R.id.signuplayout);
         signupLayout.setVisibility(View.INVISIBLE);
-        //-----------------------------------
         animationSlideLeft = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation_leave);
         animationSlideLeft.setAnimationListener(this);
         animationSlideRight = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation_enter);
         animationSlideLeftS = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.animation_enter_right);
         animationSlideExit = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.animation_leave_right);
         animationSlideExit.setAnimationListener(this);
-        //------------------------------------
     }
 
     public void login(View view) {
@@ -135,5 +137,94 @@ public class LandingActivity extends AppCompatActivity implements Animation.Anim
             System.exit(1);
         }
     }
+
+    public void checkLogin(View view){
+
+        loginPasswordEditText = (EditText)findViewById(R.id.password);
+        loginEmailEditText = (EditText)findViewById(R.id.email);
+        loginEmail = loginEmailEditText.getText().toString();
+        loginPassword = loginPasswordEditText.getText().toString();
+        String usps = loginEmail + " " + loginPassword;
+        if(loginPassword.compareTo("") != 0 || loginEmail.compareTo("") != 0){
+            Intent intent = new Intent(this, Homepage.class);
+            intent.putExtra(EXTRA_MESSAGE, usps);
+            startActivity(intent);
+            finish();
+        }
+        else{
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Login Failed")
+                    .setMessage("Username or Password was incorrect.")
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    })
+                    .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+
+    }
+    public void checkSignUp(View view){
+        SUPWEditText = (EditText)findViewById(R.id.passwordS);
+        SUEmailEditText = (EditText)findViewById(R.id.emailS);
+        SULNEditText = (EditText)findViewById(R.id.lastname);
+        SUFNEditText = (EditText)findViewById(R.id.firstname);
+
+        SUEmail= SUEmailEditText.getText().toString();
+        SUPassword = SUPWEditText.getText().toString();
+        SUFN = SUFNEditText.getText().toString();
+        SULN = SULNEditText.getText().toString();
+
+        String usps = SUEmail + " " + SUPassword + " " + SUFN + " " + SULN;
+        if(SUPassword.compareTo("") != 0 || SUEmail.compareTo("") != 0 || SUFN.compareTo("") != 0 || SULN.compareTo("") != 0){
+            final Intent intent = new Intent(this, Homepage.class);
+            new AlertDialog.Builder(this)
+                    .setTitle("Account Created")
+                    .setMessage("Account Creation Successful.")
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            startActivity(intent);
+                            finish();
+                        }
+                    })
+                    /*.setNegativeButton(android.R.string., new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    })*/
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+            intent.putExtra(EXTRA_MESSAGE, usps);
+            startActivity(intent);
+            finish();
+        }
+        else{
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Account Creation Failed")
+                    .setMessage("Account Creation Failed")
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    })
+                    /*.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    })*/
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+    }
+
+
 
 }
