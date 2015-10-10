7fa66ac1de02f5a6bc53f5e23527859916885940
DrChitwood <aschitwo@uark.edu>
2015-10-10 10:18:12 -0500

    Try this again

---
+++ Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/LandingActivity.java
@@ -0,0 +1,14 @@
+package com.dine_n_wineinc.dine_n_wine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+        getActionBar().hide();
+    }
+}
