a8aeb630d1b0c5a4390b8d3c1c5d4377767e08a1
DrChitwood <aschitwo@uark.edu>
2015-10-10 13:47:40 -0500

    Finished Landing, Transitions

--- Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/LandingActivity.java
+++ Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/LandingActivity.java
@@ -1,8 +1,12 @@
 package com.dine_n_wineinc.dine_n_wine;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -13,4 +17,17 @@ public class LandingActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
     }
+
+    public void login(View view) {
+
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        this.overridePendingTransition(R.anim.animation_enter, R.anim.animation_leave);
+
+    }
+
+    public void signUp(View view) {
+
+    }
+
 }
