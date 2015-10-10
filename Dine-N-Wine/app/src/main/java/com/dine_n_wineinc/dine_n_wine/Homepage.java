1285d8369a14d4fd98339987fa987230be7f2c27
DrChitwood <aschitwo@uark.edu>
2015-10-10 18:36:49 -0500

    Added action bar to Homepage

--- Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/Homepage.java
+++ Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/Homepage.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -14,6 +16,9 @@ public class Homepage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_homepage);
