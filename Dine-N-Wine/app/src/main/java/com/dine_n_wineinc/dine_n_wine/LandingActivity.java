4c4e7bb84fec09335c8780182c88f11534b4a7f2
DrChitwood <aschitwo@uark.edu>
2015-10-10 15:18:26 -0500

    Smoothing Transition

--- Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/LandingActivity.java
+++ Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/LandingActivity.java
@@ -6,28 +6,74 @@ import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Slide;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class LandingActivity extends AppCompatActivity {
+public class LandingActivity extends AppCompatActivity implements Animation.AnimationListener{
 
+
+    TextView textView;
+    LinearLayout linearLayout;
+    RelativeLayout loginLayout;
+    //--------------------------------
+    Animation animationSlideLeft;
+    Animation animationSlideRight;
+    //---------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+        textView = (TextView)findViewById(R.id.welcomeText);
+        linearLayout = (LinearLayout)findViewById(R.id.buttons);
+        loginLayout = (RelativeLayout)findViewById(R.id.loginlayout);
+        loginLayout.setVisibility(View.INVISIBLE);
+        //-----------------------------------
+        animationSlideLeft = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation_leave);
+        animationSlideLeft.setAnimationListener(this);
+        animationSlideRight = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation_left_edge_enter);
+        //------------------------------------
     }
 
     public void login(View view) {
 
-        Intent i = new Intent(this, LoginActivity.class);
-        startActivity(i);
-        this.overridePendingTransition(R.anim.animation_enter, R.anim.animation_leave);
+        textView.startAnimation(animationSlideLeft);
+        linearLayout.startAnimation(animationSlideLeft);
 
+
+        //Intent i = new Intent(this, LoginActivity.class);
+        //startActivity(i);
+        //this.overridePendingTransition(R.anim.animation_enter, R.anim.animation_leave);
     }
 
     public void signUp(View view) {
 
     }
 
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+        if(animation == animationSlideLeft) {
+            textView.setVisibility(View.INVISIBLE);
+            linearLayout.setVisibility(View.INVISIBLE);
+            loginLayout.setVisibility(View.VISIBLE);
+            loginLayout.startAnimation(animationSlideRight);
+        }
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
 }
