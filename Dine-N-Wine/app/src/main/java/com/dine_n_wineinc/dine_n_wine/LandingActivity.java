15e64239d99c8ed8537ba5697ccda779c921c5b0
Moises Guerra <moises_guerra@yahoo.com>
2015-10-10 15:09:50 -0500

    Add skip button and homepage_version1

--- Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/LandingActivity.java
+++ Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/LandingActivity.java
@@ -3,9 +3,14 @@ package com.dine_n_wineinc.dine_n_wine;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class LandingActivity extends AppCompatActivity {
 
+    public final static String EXTRA_MESSAGE = "com.dine_n_wineinc.dine_n_wine.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,4 +18,19 @@ public class LandingActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
     }
+
+    /** Called when the user clicks the Send button */
+
+    public void homepage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, Homepage.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+
+        String message = "Aloha!";
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
