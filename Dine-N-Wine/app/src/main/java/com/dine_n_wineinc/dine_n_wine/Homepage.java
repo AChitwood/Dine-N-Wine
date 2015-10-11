ac1794e70a0f04bc232b1bb72145ff0509d60193
DrChitwood <aschitwo@uark.edu>
2015-10-11 00:30:08 -0500

    No Changes
    just fuckin' around

--- Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/Homepage.java
+++ Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/Homepage.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -16,7 +17,6 @@ public class Homepage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
 
 
         super.onCreate(savedInstanceState);
@@ -32,5 +32,6 @@ public class Homepage extends AppCompatActivity {
         setContentView(textView);
 
     }
+
 
 }
