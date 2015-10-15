11ab6ceb787abb620bb13b6888e4cd004c38574a
DrChitwood <aschitwo@uark.edu>
2015-10-15 15:11:35 -0500

    This is like 3 days of work

--- Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/Homepage.java
+++ Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/Homepage.java
@@ -1,19 +1,11 @@
 package com.dine_n_wineinc.dine_n_wine;
 
-import android.support.v4.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 
 public class Homepage extends AppCompatActivity {
@@ -22,21 +14,33 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_homepage);
-        Intent intent = getIntent();
-        /*
-        String message = intent.getStringExtra(LandingActivity.EXTRA_MESSAGE);
-        TextView textView = new TextView(this);
-
-        textView.setTextSize(40);
-        textView.setText(message);
-        setContentView(textView);
-
-        if (savedInstanceState == null) {
+        setContentView(R.layout.activity_homepage);
+        if(savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container, new PlaceholderFragment()).commit();
-        } */
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {}
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_homepage, container, false);
+            //ListView listView = (ListView) rootView.findViewById(R.id.search_list);
+            //listView.setAdapter()
+
+            return rootView;
+        }
 
     }
 
 }
+
+
+
