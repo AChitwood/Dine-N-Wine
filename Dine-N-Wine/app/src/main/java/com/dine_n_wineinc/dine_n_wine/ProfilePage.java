01ae6bc36ae6e977a38d74f186be8a7d9fd52352
anfelder <andrewfelder95@gmail.com>
2015-10-15 22:03:27 -0500

    started profile page activity

---
+++ Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/ProfilePage.java
@@ -0,0 +1,15 @@
+package com.dine_n_wineinc.dine_n_wine;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ProfilePage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+    }
+
+}
