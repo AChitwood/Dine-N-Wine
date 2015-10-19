e70a2b835c32388d81e79df5b8cbf98ad65cc487
DrChitwood <aschitwo@uark.edu>
2015-10-19 14:44:43 -0500

    Starting Over on the homepage

--- Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/Homepage.java
+++ Dine-N-Wine/app/src/main/java/com/dine_n_wineinc/dine_n_wine/Homepage.java
@@ -1,46 +1,13 @@
 package com.dine_n_wineinc.dine_n_wine;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-
-public class Homepage extends AppCompatActivity {
+public class Homepage extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_homepage);
-        if(savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-
-
     }
-
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {}
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_homepage, container, false);
-            //ListView listView = (ListView) rootView.findViewById(R.id.search_list);
-            //listView.setAdapter()
-
-            return rootView;
-        }
-
-    }
-
 }
-
-
-
